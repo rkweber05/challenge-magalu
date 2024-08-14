9337c6ee0b42d0459e003da82dc7fabf33ce0299
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-08-14 16:40:13 -0300

    database connection with docker

---
+++ src/main/java/tech/buildrun/challengemagalu/ChallengeMagaluApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.challengemagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeMagaluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeMagaluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/buildrun/challengemagalu/ChallengeMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.challengemagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeMagaluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
